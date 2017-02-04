479f4f4e59bcd1c8cdde1b3f7ef1d2de036e5c70
Wei Sun <wsun@CNwsun.local>
2017-02-04 09:53:57 +0800

    add for no2; Wayde

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'no2');
+define('DB_NAME', 'database_name_here');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'root');
+define('DB_USER', 'username_here');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'Hakula567');
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL主机 */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
